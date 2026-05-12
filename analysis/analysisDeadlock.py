a05e2ce9add6f563ea2830870195468870600796
Eric Wegener <wee34122@hs/regensburg.de>
2026-05-12 16:17:50 +0200

    added more analysis functions to evaluate the results

--- analysis/analysisDeadlock.py
+++ analysis/analysisDeadlock.py
@@ -1,8 +1,3 @@
-#Success-Rate
-#Retry-Distribution
-#Latency-Comparison
-#Quartils
-#Retry-Overhead
 #Throughput
 #Influence of Workload
 #Influence of Delay
@@ -61,10 +56,81 @@ def _success_rates(data):
     print("\n=== total ===")
     print(total_rate)
 
-#def _retry_Distribution():
+def _retry_distribution(data):
+    df = data.copy()
+    df_success = df[df["status"] == "success"]
+    df_success["r"] = df_success["run_id"].str.extract(r"_(r\d+)_")
+    result = df_success.groupby("r").size().sort_index()
 
+    result.index = "r" + result.index
+    result.index.name = None
+
+    print("\n=== retry-distribution ===")
+    print(result)
+
+def _latency_comparison(data):
+    df = data.copy()
+    df_success = df[df["status"] == "success"]
+    df_success["s"] = df_success["run_id"].str.extract(r"(s\d+)_")
+
+    success = df_success.groupby("s").size().sort_index()
+    success.index.name = None
+
+    avg = (df_success.groupby("s")["total_ms"].mean())
+    avg.index.name = None
+
+    #print(success)
+    print(avg)
+
+def _quartiles(data):
+    df = data.copy()
+    df_success = df[df["status"] == "success"]
+    df_success["s"] = df_success["run_id"].str.extract(r"(s\d+)_")
+
+    df_failed = df[df["status"] == "failed"]
+    df_failed["f"] = df_failed["run_id"].str.extract(r"(s\d+)_")
+
+    quantiles_success = df_success["total_ms"].quantile([0.50, 0.95, 0.99])
+    quantiles_failed = df_failed["total_ms"].quantile([0.50, 0.95, 0.99])
+
+    print(quantiles_success)
+    print("")
+    print(quantiles_failed)
+
+def _retry_overhead(data):
+    df_overhead = data.copy()
+
+    df_overhead["s"] = df_overhead["run_id"].str.extract(r"(s\d+)_")
+    df_overhead["cc"] = df_overhead["run_id"].str.extract(r"_(cc\d+)")
+    df_overhead["count"] = df_overhead["run_id"].str.extract(r"_(r\d+)_")
+
+    df_overhead["attempts_trimmed"] = (df_overhead["attempts_ms"].
+                                       apply(lambda x: x[1:] if isinstance(x, list) and len(x) > 1 else []))
+    df_overhead["best_ms"] = (df_overhead["attempts_trimmed"].
+                                      apply(lambda x: min(x) if len(x) > 0 else 0))
+    df_overhead["overhead"] = df_overhead["total_ms"] - df_overhead["best_ms"]
+
+    s_avg = df_overhead.groupby(["s", "status"])["overhead"].mean()
+    cc_avg = df_overhead.groupby(["cc", "status"])["overhead"].mean()
+    rc_avg = df_overhead.groupby(["count", "status"])["overhead"].mean()
+
+    print("=== strategy ===")
+    print(s_avg)
+
+    print("")
+    print("=== concurrency ===")
+    print(cc_avg)
+
+    print("")
+    print("=== retry count ===")
+    print(rc_avg)
 
 def start_analysis():
     data = _get_data()
 
-    _success_rates(data)
+    #_success_rates(data)
+    print("")
+    #_retry_distribution(data)
+    #_latency_comparison(data)
+    #_quartiles(data)
+    #_retry_overhead(data)
